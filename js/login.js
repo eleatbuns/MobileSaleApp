c33cd4748f1fb8c502c62f5cb05932db6e81fc4f
GaoLi <gaoli199512@163.com>
2018-03-21 10:09:20 +0800

    Start the user management homepage.

--- js/login.js
+++ js/login.js
@@ -14,24 +14,30 @@ app.controller("sale-app-controller", function($scope, $http) {
 	$scope.loginAuthentication = function() {
 		$http({
 			method: "POST",
-			url: "http://localhost:8080/sale/login/",
+			url: "http://211.87.233.74:8080/sale/login/",
 			params: ({
 				"useraccount": $scope.useraccount,
 				"password": $scope.password
 			})
 		}).then(function successCallback(response) {
-				returnMessage($scope,response.data.result);
+				returnMessage($scope, response.data.result);
 			},
 			function errorCallback(response) {
-				console.log("error:" + response)
+				console.log("error:" + response);
 			});
 	}
+	$scope.jumpToUrl = function(msg) {
+		var hre = 'html/CustomerManagement/homepage.html?msg=' + angular.toJson(msg);
+		//传递对象：先将对象转成字符串（序列化）
+		location.href = hre;
+	};
 });
 
-function returnMessage($scope,returndata) {
+function returnMessage($scope, returndata) {
 	switch(returndata) {
 		case VERIFICATIONSUCCESS:
 			$scope.returnMessage = SUCCESSMESSAGE;
+			$scope.jumpToUrl($scope.useraccount);
 			break;
 		case ACCOUNTISNULL:
 			$scope.returnMessage = ACCOUNTNOTEXIST;
