f6fe547d1a1d27ec185934b066f50a6e40c89cbe
GaoLi <gaoli199512@163.com>
2018-04-09 10:46:21 +0800

    modify homepage style,remove map,add listView

--- js/login.js
+++ js/login.js
@@ -11,10 +11,18 @@ const INCORRCETPASSWORD = "密码输入错误";
 var app = angular.module("sale-app", []);
 app.controller("sale-app-controller", function($scope, $http) {
 
+	angular.element(document).ready(function() {
+		var storage = window.localStorage;
+		if(storage.getItem("useraccount") != null && storage.getItem("password") != null) {
+			var hre = 'html/Customer/homepage.html?useraccount=' + storage.getItem("useraccount");
+			window.location = hre;
+		}
+	});
+
 	$scope.loginAuthentication = function() {
 		$http({
-			method: "POST",
-			url: "http://211.87.233.74:8080/sale/login/",
+			method: "GET",
+			url: "http://192.168.137.1:8080/sale/login/",
 			params: ({
 				"useraccount": $scope.useraccount,
 				"password": $scope.password
@@ -27,22 +35,26 @@ app.controller("sale-app-controller", function($scope, $http) {
 			});
 	}
 	$scope.jumpToUrl = function(msg) {
-		var hre = 'html/CustomerManagement/homepage.html?msg=' + angular.toJson(msg);
-		//传递对象：先将对象转成字符串（序列化）
+		var hre = 'html/Customer/homepage.html?useraccount=' + msg;
 		location.href = hre;
 	};
+
 });
 
 function returnMessage($scope, returndata) {
 	switch(returndata) {
 		case VERIFICATIONSUCCESS:
+			//验证成功
 			$scope.returnMessage = SUCCESSMESSAGE;
+			rememberUser($scope);
 			$scope.jumpToUrl($scope.useraccount);
 			break;
 		case ACCOUNTISNULL:
+			//账户为空
 			$scope.returnMessage = ACCOUNTNOTEXIST;
 			break;
 		case PASSWORDERROR:
+			//密码错误
 			$scope.returnMessage = INCORRCETPASSWORD;
 			break;
 		default:
@@ -66,3 +78,13 @@ $(document).ready(function() {
 		$("#password-hr").css("width", "0");
 	});
 });
+
+function rememberUser($scope) {
+	if(!window.localStorage) {
+		console.log("无法使用localstorage");
+	} else {
+		var storage = window.localStorage;
+		storage.setItem("useraccount", $scope.useraccount);
+		storage.setItem("password", $scope.password);
+	}
+}
